676a99758e6c004aa93c56c1e1fac6b7f8ff8fe4
Marco Massimi Alunni <marcomassimi08@gmail.com>
2026-02-13 09:59:33 +0100

    Aggiunta Versione V3
    aggiunta la versione V3 con le classi per la conversione e modificata la classe ConversioneRequest

---
+++ src/serverrest/ConversioneRequest.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package serverrest;
+
+/**
+ *
+ * @author delfo
+ */
+public class ConversioneRequest {
+    private double operando;
+    private String unitaMisura1;
+    private String unitaMisura2;
+    
+    // Costruttore vuoto necessario per GSON
+    public ConversioneRequest() {
+    }
+    
+    // Costruttore con parametri
+    public ConversioneRequest(double operando, String unitaMisura1, String unitaMisura2) {
+        this.operando=operando;
+        this.unitaMisura1=unitaMisura1;
+        this.unitaMisura2=unitaMisura2;
+    }
+
+    public double getOperando() {
+        return operando;
+    }
+
+    public String getUnitaMisura1() {
+        return unitaMisura1;
+    }
+
+    public String getUnitaMisura2() {
+        return unitaMisura2;
+    }
+    
+   
+    
+    @Override
+    public String toString() {
+        return "ConversioneRequest{" +
+                "unita1= " + unitaMisura1 +
+                ", unita2= " + unitaMisura2 +
+                ", operando= " + operando + '\n' +
+                '}';
+    }
+}
